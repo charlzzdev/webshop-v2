979c13c55996540b43b463805b9da2824a1d188d
Charles <ellerkaresz@gmail.com>
2019-06-22 23:39:18 +0200

    create Header

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const menuIcon = <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 cursor-pointer block sm:hidden" onClick={() => setMenuOpen(!menuOpen)} viewBox="0 0 24 24" fill="none" stroke="#000" strokeWidth="2.25" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>;
+
+  return (
+    <header className="h-16 px-8 lg:px-32 xl:px-64 flex justify-between items-center bg-gray-400 shadow-lg">
+      <a href="/" className="text-xl text-gray-900">Webshop</a>
+      {menuIcon}
+      <ul className={
+        `${menuOpen ?
+          "flex flex-col-reverse justify-end bg-gray-400 shadow-lg absolute left-0 bottom-0 h-full w-3/4"
+          :
+          "hidden sm:flex"}`
+      }>
+        <li className="px-4 py-3 text-gray-700 hover:text-gray-900">
+          <a href="/">About</a>
+        </li>
+        <li className="px-4 py-3 text-gray-700 hover:text-gray-900">
+          <a href="/">
+            Cart <span className="w-6 h-6 ml-2 inline-block bg-teal-400 text-center rounded-full">0</span>
+          </a>
+        </li>
+        <li className="px-4 py-3 text-gray-700 hover:text-gray-900">
+          <a href="/">
+            Login
+          </a>
+        </li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header;
