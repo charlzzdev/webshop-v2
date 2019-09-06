2f47e5d37abc422f2aef86d2da6d124044e80fcc
Charles <ellerkaresz@gmail.com>
2019-09-06 19:47:51 +0200

    add ability to change password

--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -17,6 +17,18 @@ const ProfilePage = ({user}) => {
       .catch(err => setStatus({color: 'bg-red-400', msg: err.message}));
   }
 
+  const updatePassword = e => {
+    e.preventDefault();
+
+    const [newPass, newPassAgain] = e.target;
+
+    if(newPass.value === newPassAgain.value){
+      user.updatePassword(newPass.value)
+        .then(() => setStatus({color: 'bg-green-400', msg: 'Your password has been updated successfully.'}))
+        .catch(err => setStatus({color: 'bg-red-400', msg: err.message}));
+    } else setStatus({color: 'bg-red-400', msg: 'Passwords do not match.'});
+  }
+
   return (
     <div className="w-3/4 mx-auto">
       <Info color={status.color} msg={status.msg} setStatus={setStatus} />
@@ -32,7 +44,21 @@ const ProfilePage = ({user}) => {
         />
         {updatedInfo.email && <button onClick={updateEmail} className="px-2 py-1 ml-4 bg-green-300 hover:bg-green-400">Update email</button>}
       </div>
-      <button className="block px-2 py-1 mt-4 bg-gray-300 hover:bg-gray-400">Change password</button>
+      <button
+        onClick={() => setUpdatedInfo({passwordToggled: !updatedInfo.passwordToggled})}
+        className="block px-2 py-1 mt-4 bg-gray-300 hover:bg-gray-400"
+      >Change password</button>
+      <form onSubmit={updatePassword}>
+        {
+          updatedInfo.passwordToggled && (
+            <>
+              <input type="password" placeholder="New password" autoComplete="" className="block p-2 mt-2 bg-gray-100" />
+              <input type="password" placeholder="New password again" autoComplete="" className="block p-2 mt-2 bg-gray-100" />
+              <button className="block px-2 py-1 mt-4 bg-green-300 hover:bg-green-400" >Confirm</button>
+            </>
+          )
+        }
+      </form>
 
       <h1 className="text-4xl border-b mb-4 mt-8">Uploaded products</h1>
       <div className="uploaded-products"></div>
