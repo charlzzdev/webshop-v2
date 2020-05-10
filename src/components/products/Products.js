1a4de5fefa866287716a9b860b623ae469a0cff9
Charles <ellerkaresz@gmail.com>
2020-05-10 18:25:16 +0200

    improve sorting by category

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -48,43 +48,60 @@ const Products = ({ match, history }) => {
   const redOrGreen = (bool) => bool ? 'green' : 'red';
 
   return (
-    <div className="flex flex-wrap justify-center relative mt-20">
-      <button onClick={() => history.goBack()} className="absolute" style={{ top: "-40px" }}><svg width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#1a202c" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"><path d="M19 12H6M12 5l-7 7 7 7" /></svg></button>
-      <div className="fixed left-0 hidden sm:block">
-        <Link to="/products/phones" className="nav-icon"><PhonesIcon /></Link>
-        <Link to="/products/computers" className="nav-icon"><ComputersIcon /></Link>
-        <Link to="/products/accessories" className="nav-icon"><AccessoriesIcon /></Link>
-        <Link to="/products/consoles" className="nav-icon"><ConsolesIcon /></Link>
-      </div>
-      {
-        sortedItems.map(product => (
-          <Link to={`/products/view/${product.id}`} key={product.id} className="max-w-sm m-3 rounded overflow-hidden shadow-lg">
-            <div className="overflow-hidden" style={{ width: '300px', height: '200px' }}>
-              {!images[product.id] && <LoadingSpinner />}
-              <img src={images[product.id]} alt={product.name} />
-            </div>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">{product.name}</div>
-              <p className="text-gray-700 text-base">
-                {product.description}
-              </p>
-            </div>
-            <div className="px-6 py-4">
-              {daysSince(product.releaseDate) < -30 ? '' : (
-                <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-green-700 mr-2">
-                  New
-                </span>
-              )}
-
-              <span
-                className={`inline-block bg-${redOrGreen(product.inStock)}-200 rounded-full px-3 py-1 text-sm font-semibold text-${redOrGreen(product.inStock)}-700 mr-2`}
-              >
-                {product.inStock ? 'In Stock' : 'Out of Stock'}
-              </span>
-            </div>
+    <div>
+      <div className="text-center mt-8">
+        <button onClick={() => history.goBack()} style={{ top: "-40px" }}><svg width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#1a202c" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"><path d="M19 12H6M12 5l-7 7 7 7" /></svg></button>
+        <div className="hidden sm:flex justify-center mt-4">
+          <Link to="/products/phones" className="mx-4 flex flex-col items-center">
+            <PhonesIcon size="32" />
+            {match.params.product === 'phones' ? <strong>Phones</strong> : 'Phones'}
           </Link>
-        ))
-      }
+          <Link to="/products/computers" className="mx-4 flex flex-col items-center">
+            <ComputersIcon size="32" />
+            {match.params.product === 'computers' ? <strong>Computers</strong> : 'Computers'}
+          </Link>
+          <Link to="/products/accessories" className="mx-4 flex flex-col items-center">
+            <AccessoriesIcon size="32" />
+            {match.params.product === 'accessories' ? <strong>Accessories</strong> : 'Accessories'}
+          </Link>
+          <Link to="/products/consoles" className="mx-4 flex flex-col items-center">
+            <ConsolesIcon size="32" />
+            {match.params.product === 'consoles' ? <strong>Consoles</strong> : 'Consoles'}
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap justify-center relative mt-8">
+        {
+          sortedItems.map(product => (
+            <Link to={`/products/view/${product.id}`} key={product.id} className="max-w-sm m-3 rounded overflow-hidden shadow-lg">
+              <div className="overflow-hidden" style={{ width: '300px', height: '200px' }}>
+                {!images[product.id] && <LoadingSpinner />}
+                <img src={images[product.id]} alt={product.name} />
+              </div>
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{product.name}</div>
+                <p className="text-gray-700 text-base">
+                  {product.description}
+                </p>
+              </div>
+              <div className="px-6 py-4">
+                {daysSince(product.releaseDate) < -30 ? '' : (
+                  <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-green-700 mr-2">
+                    New
+                  </span>
+                )}
+
+                <span
+                  className={`inline-block bg-${redOrGreen(product.inStock)}-200 rounded-full px-3 py-1 text-sm font-semibold text-${redOrGreen(product.inStock)}-700 mr-2`}
+                >
+                  {product.inStock ? 'In Stock' : 'Out of Stock'}
+                </span>
+              </div>
+            </Link>
+          ))
+        }
+      </div>
     </div>
   )
 }
